cfc2789a80e1b8862170ea8fc84aa1a92248698e
Pieter Wuille <pieter.wuille@gmail.com>
2012-06-23 14:17:13 +0200

    Preliminary undo file creation
    Create files (one per block) with undo information for the transactions
    in it.

--- src/main.h
+++ src/main.h
@@ -154,6 +154,10 @@ public:
         return GetDirectory(base) / strprintf("%08u%s.blk", nHeight, GetAlternative().c_str());
     }
 
+    boost::filesystem::path GetUndoFile(const boost::filesystem::path &base) const {
+        return GetDirectory(base) / strprintf("%08u%s.und", nHeight, GetAlternative().c_str());
+    }
+
     // TODO: make thread-safe (lockfile, atomic file creation, ...?)
     void MakeUnique(const boost::filesystem::path &base) {
         while (boost::filesystem::exists(GetFileName(base)))
@@ -836,6 +840,18 @@ public:
 
     IMPLEMENT_SERIALIZE(
         READWRITE(vprevout);
+    )
+};
+
+class CBlockUndo
+{
+public:
+    uint256 coinbaseHash;
+    std::vector<CTxUndo> vtxundo;
+
+    IMPLEMENT_SERIALIZE(
+        READWRITE(coinbaseHash);
+        READWRITE(vtxundo);
     )
 };
 
