75710d950d07c9ce9a277c8e506fcb9fec4f11c9
Pieter Wuille <pieter.wuille@gmail.com>
2014-01-10 13:23:26 +0100

    Per-peer block download tracking and stalled download detection.
    Keep track of which block is being requested (and to be requested) from
    each peer, and limit the number of blocks in-flight per peer. In addition,
    detect stalled downloads, and disconnect if they persist for too long.
    
    This means blocks are never requested twice, and should eliminate duplicate
    downloads during synchronization.

--- src/main.h
+++ src/main.h
@@ -57,6 +57,9 @@ static const int COINBASE_MATURITY = 100;
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 /** Maximum number of script-checking threads allowed */
 static const int MAX_SCRIPTCHECK_THREADS = 16;
+/** Number of blocks that can be requested at any given time from a single peer. */
+static const int MAX_BLOCKS_IN_TRANSIT_PER_PEER = 128;
+
 #ifdef USE_UPNP
 static const int fHaveUPnP = true;
 #else
@@ -180,6 +183,9 @@ bool VerifySignature(const CCoins& txFrom, const CTransaction& txTo, unsigned in
 bool AbortNode(const std::string &msg);
 /** Get statistics from node state */
 bool GetNodeStateStats(NodeId nodeid, CNodeStateStats &stats);
+/** Increase a node's misbehavior score. */
+void Misbehaving(NodeId nodeid, int howmuch);
+
 
 /** (try to) add transaction to memory pool **/
 bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState &state, const CTransaction &tx, bool fLimitFree,
