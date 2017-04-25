5e9e1e9a80298dcbb8e7a486cc432271f153b5f7
Pieter Wuille <pieter.wuille@gmail.com>
2017-04-25 11:29:19 -0700

    Store/allow tx metadata in all undo records
    Previously, transaction metadata (height, coinbase or not, and before
    the previous commit also nVersion) was only stored for undo records
    that correspond to the last output of a transaction being spent.
    
    This only saves 2 bytes per undo record. Change this to storing this
    information for every undo record, and stop complaining for having it
    in non-last output spends. This means that undo dat written with
    this patch won't be readable by older versions anymore.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1278,11 +1278,9 @@ void UpdateCoins(const CTransaction& tx, CCoinsViewCache& inputs, CTxUndo &txund
             // mark an outpoint spent, and construct undo information
             txundo.vprevout.push_back(CTxInUndo(coins->vout[nPos]));
             coins->Spend(nPos);
-            if (coins->vout.size() == 0) {
-                CTxInUndo& undo = txundo.vprevout.back();
-                undo.nHeight = coins->nHeight;
-                undo.fCoinBase = coins->fCoinBase;
-            }
+            CTxInUndo& undo = txundo.vprevout.back();
+            undo.nHeight = coins->nHeight;
+            undo.fCoinBase = coins->fCoinBase;
         }
     }
     // add outputs
@@ -1504,12 +1502,15 @@ bool ApplyTxInUndo(const CTxInUndo& undo, CCoinsViewCache& view, const COutPoint
 
     CCoinsModifier coins = view.ModifyCoins(out.hash);
     if (undo.nHeight != 0) {
-        // undo data contains height: this is the last output of the prevout tx being spent
-        if (!coins->IsPruned()) fClean = false;
-        coins->Clear();
+        if (!coins->IsPruned()) {
+            // verify that the undo data matches the utxo tx metadata
+            if (coins->fCoinBase != undo.fCoinBase || (uint32_t)coins->nHeight != undo.nHeight) fClean = false;
+        }
+        // restore height/coinbase tx metadata from undo data
         coins->fCoinBase = undo.fCoinBase;
         coins->nHeight = undo.nHeight;
     } else {
+        // undo data does not contain height/coinbase: it cannot be the last spent output of a tx
         if (coins->IsPruned()) fClean = false;
     }
     if (coins->IsAvailable(out.n)) fClean = false;
